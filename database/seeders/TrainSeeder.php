982292005dffc04a6bb408e2f1404ff3404cb971
Federico Airoldi <airoldibusiness@gmail.com>
2023-06-13 02:41:09 +0200

    FASE 3 - inserisco dati attraverso seeder con Faker

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for( $i = 0; $i < 10; $i++ ){
+            $newTrain = new Train();
+            $newTrain->azienda = $faker->company;
+            $newTrain->stazione_partenza = $faker->city;
+            $newTrain->stazione_arrivo = $faker->city;
+            $newTrain->orario_partenza = $faker->time;
+            $newTrain->orario_arrivo = $faker->time;
+            $newTrain->codice_treno = $faker->unique()->bothify('###??#');
+            $newTrain->numero_carrozze = $faker->numberBetween(1, 10);
+            $newTrain->in_orario = $faker->boolean;
+            $newTrain->cancellato = $faker->boolean;
+            $newTrain->save();
+        }
+    }
+}
