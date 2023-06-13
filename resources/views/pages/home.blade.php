4aac4d56b140c07ffb17c27d5f634fca319c1e25
Federico Airoldi <airoldibusiness@gmail.com>
2023-06-13 03:07:18 +0200

    concluso - stampo tutto nella pagina nelle cards, sistemo lo stile

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,10 +6,31 @@
 
 @section( 'content' )
 
+<h1 class="text-center text-uppercase fs-1 fw-bolder text-light mt-4">TRAINS</h1>
 
 
 
+<div class="maincore col-8 m-auto d-flex flex-wrap gap-4 h-100 p-4 bg-dark justify-content-center">
+
+    @foreach ($trains as $train)
+    <div class="card col-3 ">
+        <img src="{{ Vite::asset('resources/images/trainimage.jpg') }}" class="card-img-top" alt="...">
+
+        <div class="card-body p-3">
+            <p class="card-text"><strong>Azienda:</strong> {{ $train->azienda }}</p>
+            <p class="card-text"><strong>Stazione di partenza:</strong> {{ $train->stazione_partenza }}</p>
+            <p class="card-text"><strong>Stazione di arrivo:</strong> {{ $train->stazione_arrivo }}</p>
+            <p class="card-text"><strong>Orario di partenza:</strong> {{ $train->orario_partenza }} <br> <strong>Orario di arrivo:</strong> {{ $train->orario_arrivo }}</p>
+            <p class="card-text"><strong>Codice treno:</strong> {{ $train->codice_treno }}</p>
+            <p class="card-text"><strong>Numero carrozze:</strong> {{ $train->numero_carrozze }}</p>
+            <p class="card-text"><strong>In orario:</strong> {{ $train->in_orario }}</p>
+            <p class="card-text"><strong>Cancellato:</strong> {{ $train->cancellato }}</p>
+        </div>
+    </div>
+@endforeach
 
 
+
+</div>
 
 @endsection
